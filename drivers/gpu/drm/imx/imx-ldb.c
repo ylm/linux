0585c7a042dc6c7e5f8ed5bd6d7a20a1a993ea81
Sean Cross <xobs@kosagi.com>
2016-02-11 16:01:12 +0800

    DRM: imx: ldb: always restart ldb device
    Always write "0" to the ldb control registers before writing
    the actual, wanted values.
    
    The hope is that this will cause the two channels to have synced
    clocks, even if they were set up askew in the bootloader.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>
    
    (sakaki: modify context for hunk 2, for 4.7.2)

--- drivers/gpu/drm/imx/imx-ldb.c
+++ drivers/gpu/drm/imx/imx-ldb.c
@@ -166,6 +166,10 @@ static void imx_ldb_set_clock(struct imx_ldb *ldb, int mux, int chno,
 		unsigned long serial_clk, unsigned long di_clk)
 {
 	int ret;
+	int ctrl;
+
+	regmap_read(ldb->regmap, IOMUXC_GPR2, &ctrl);
+	dev_dbg(ldb->dev, "%d:%s: LDB_CTRL: 0x%08x\n", chno, __func__, ctrl);
 
 	dev_dbg(ldb->dev, "%d:%s: now: %ld want: %ld\n", chno, __func__,
 			clk_get_rate(ldb->clk_pll[chno]), serial_clk);
@@ -241,6 +245,10 @@ static void imx_ldb_encoder_enable(struct drm_encoder *encoder)
 				   mux << lvds_mux->shift);
 	}
 
+	regmap_read(ldb->regmap, IOMUXC_GPR2, &reg);
+	dev_dbg(ldb->dev, "%s: LDB_CTRL: 0x%08x\n", __func__, reg);
+	regmap_write(ldb->regmap, IOMUXC_GPR2, 0);
+	udelay(100);
 	regmap_write(ldb->regmap, IOMUXC_GPR2, ldb->ldb_ctrl);
 
 	drm_panel_enable(imx_ldb_ch->panel);
