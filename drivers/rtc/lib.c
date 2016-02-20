b4cdb905c667e6ccbc7b4d01214a8e4d54767268
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2016-02-20 04:54:58 +0100

    rtc: Add an option to invalidate dates in 2038
    hctosys is setting the system time from the kernel. This means that 32bit
    system can get their time set to a date after the 31bit time_t overflow.
    
    This is currently an issue as userspace is not yet ready to handle those
    dates and may break. For example systemd's usage of timerfd shows that the
    timerfd will always fire immediately because it can't be set at a date
    after the current date.
    
    The new RTC_INVALID_2038 option will make sure that date after 03:09:07 on
    Jan 19 2038 are invalid. This is 5 minutes before the 31bit overflow. This
    leaves enough time for userspace to react and is short enough to make the
    issue visible.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

--- drivers/rtc/lib.c
+++ drivers/rtc/lib.c
@@ -157,6 +157,10 @@ int rtc_valid_tm(struct rtc_time *tm)
 	    ((unsigned int)tm->tm_sec) >= 60)
 		return -EINVAL;
 
+#ifdef CONFIG_RTC_INVALID_2038
+	if (rtc_tm_to_time64(tm) > 0x7FFFFED4) /* 5 minutes before overflow */
+		return -EINVAL;
+#endif
 	return 0;
 }
 EXPORT_SYMBOL(rtc_valid_tm);
